f6aefcea9d3bf2581039c005ad6dfef4e824aaab
GreatPeterWu <yongbufuxing@gmail.com>
2023-09-30 22:06:17 +0800

    Site updated: 2023-09-30 22:06:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/21/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/21/hello-world/","2023/09/30/使用-Github-Pages-和-Hexo-搭建自己的独立博客/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
